a51a325afbd1842dc66d3a04502c954bb26c0a66
danbarang <danielfonoaudiologia@gmail.com>
2026-08-17 20:13:15 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        especialidades: resolve(__dirname, 'especialidades.html'),
+        programas: resolve(__dirname, 'programas.html'),
+        reservas: resolve(__dirname, 'reservas.html'),
+        sello: resolve(__dirname, 'sello.html'),
+        terapias: resolve(__dirname, 'terapias.html')
+      }
+    }
+  }
+});
